8c795c1c1b753625237a0b20f3248c8c93d0ccf1
JoZeCa1 <122526786+JoZeCa1@users.noreply.github.com>
2023-01-13 11:59:47 +0800

    Update CashOnHand.py

--- CashOnHand.py
+++ CashOnHand.py
@@ -3,7 +3,7 @@ import csv
 
 # create a file to csv file.
 # fp = Path.cwd()/"CashOnHand.csv"
-fp=Path.home()/'python4biz'/'py files'/'csv files'/'CashOnHand.csv'
+fp=Path.cwd()/'csv_report_Ignite/'CashOnHand.csv'
 
 # read the csv file to append profit and quantity from the csv.
 with fp.open(mode="r", encoding="UTF-8", newline="") as file:
